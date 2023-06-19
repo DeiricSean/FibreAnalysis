0ca02217e1b65559bd6eb90277049d3094b3761c
Derek OSullivan <djo2000@hw.ac.uk>
2023-06-19 15:35:59 +0100

    Training Image Generation

---
+++ ImageCreation/__main__.py
@@ -0,0 +1,7 @@
+
+from generate_test import test_generation_is_deterministic
+
+if __name__ == '__main__':
+    print("is this working?")
+    test_generation_is_deterministic()
+    print("perhaps")
---
+++ ImageCreation/generate_test.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from random import seed
+from generate import (
+    generate_training_example,
+    training_set,
+    Config,
+    Fibre,
+    gen_components
+)
+
+
+import numpy as np
+import math
+
+
+
+def test_generation_is_deterministic():
+    test_seed = 120
+
+    seed(test_seed)
+    data1, label1, count1 = training_set(10, Config())
+
+    seed(test_seed)
+    data2, label2, count2 = training_set(10, Config())
+
+    np.array_equal(data1, data2)
+    np.array_equal(label1, label2)
+    np.array_equal(count1, count2)
+
