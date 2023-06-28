73907f0deaf0cf436353f21ec8c6a0330415ac65
Derek OSullivan <djo2000@hw.ac.uk>
2023-06-28 14:11:52 +0100

    Segment Anything 1

--- ImageCreation/masks_to_polygons.py
+++ ImageCreation/masks_to_polygons.py
@@ -2,44 +2,49 @@
 
 # This converts image masks to polygons for use in YOLO training 
 
-# import os
-# import cv2
+import os
+import cv2
 
 
-# current_directory = os.getcwd()
-# print(current_directory)
+current_directory = os.getcwd()
+print(current_directory)
 # input_dir = os.path.join(current_directory, 'ImageCreation', 'tmp', 'masks', '')
 # output_dir = os.path.join(current_directory, 'ImageCreation', 'tmp', 'labels', '')
 
-# for j in os.listdir(input_dir):
-#     image_path = os.path.join(input_dir, j)
-#     # load the binary mask and get its contours
-#     mask = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-#     _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+input_dir = r"C:\Users\dezos\Documents\Fibres\FibreAnalysis\Data\synth\masks"
+#input_dir = r"C:\Users\dezos\Documents\Fibres\FibreAnalysis\Data\synth\temp"
+output_dir = r"C:\Users\dezos\Documents\Fibres\FibreAnalysis\Data\synth\labels"
 
-#     H, W = mask.shape
-#     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+for j in os.listdir(input_dir):
+    image_path = os.path.join(input_dir, j)
+    # load the binary mask and get its contours
+    mask = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
 
-#     # convert the contours to polygons
-#     polygons = []
-#     for cnt in contours:
-#        # if cv2.contourArea(cnt) > 200:
-#         polygon = []
-#         for point in cnt:
-#             x, y = point[0]
-#             polygon.append(x / W)
-#             polygon.append(y / H)
-#         polygons.append(polygon)
+    H, W = mask.shape
+#    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE )    
 
-#     # print the polygons
-#     with open('{}.txt'.format(os.path.join(output_dir, j)[:-4]), 'w') as f:
-#         for polygon in polygons:
-#             for p_, p in enumerate(polygon):
-#                 if p_ == len(polygon) - 1:
-#                     f.write('{}\n'.format(p))
-#                 elif p_ == 0:
-#                     f.write('0 {} '.format(p))
-#                 else:
-#                     f.write('{} '.format(p))
+    # convert the contours to polygons
+    polygons = []
+    for cnt in contours:
+       # if cv2.contourArea(cnt) > 200:
+        polygon = []
+        for point in cnt:
+            x, y = point[0]
+            polygon.append(x / W)
+            polygon.append(y / H)
+        polygons.append(polygon)
 
-#         f.close()
+    # print the polygons
+    with open('{}.txt'.format(os.path.join(output_dir, j)[:-4]), 'w') as f:
+        for polygon in polygons:
+            for p_, p in enumerate(polygon):
+                if p_ == len(polygon) - 1:
+                    f.write('{}\n'.format(p))
+                elif p_ == 0:
+                    f.write('0 {} '.format(p))
+                else:
+                    f.write('{} '.format(p))
+
+        f.close()
