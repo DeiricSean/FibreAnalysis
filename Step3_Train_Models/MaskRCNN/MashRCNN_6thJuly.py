62c738863297c7907a6f981fdd184ff981854e01
Derek OSullivan <djo2000@hw.ac.uk>
2023-07-14 18:09:28 +0100

    MaskRCNNV2

--- Step3_Train_Models/MaskRCNN/MashRCNN_6thJuly.py
+++ Step3_Train_Models/MaskRCNN/MashRCNN_6thJuly.py
@@ -20,15 +20,13 @@ from torchvision.models.detection import maskrcnn_resnet50_fpn , MaskRCNN_ResNet
 import matplotlib.pyplot as plt 
 
 # set the input height and width
-INPUT_HEIGHT = 1064
-INPUT_WIDTH = 1064
+#INPUT_HEIGHT = 1064
+#INPUT_WIDTH = 1064
 
 
 # initialize our data augmentation functions
 ChangeType = transforms.ConvertImageDtype(torch.float32)
 
-#trainTransforms = transforms.Compose([ChangeType, transforms.ToTensor()])
-#valTransforms = transforms.Compose([ChangeType, transforms.ToTensor()])
 
 trainTransforms = transforms.Compose([transforms.ToTensor()])
 valTransforms = transforms.Compose([transforms.ToTensor()])
@@ -39,8 +37,6 @@ def get_transform(train):
     transforms = []
     transforms.append(TF.pil_to_tensor())
     transforms.append(TF.convert_image_dtype(torch.float))
-    # if train:
-    #     transforms.append(RandomHorizontalFlip(0.5))
 
     return transforms.Compose(transforms)
 
@@ -56,78 +52,32 @@ def my_segmentation_transforms(image, segmentation):
 def collate_fn(batch):
     return tuple(zip(*batch))
 
-# transform = transforms.Compose(
-#     [transforms.PILToTensor(),
-#      transforms.ConvertImageDtype(torch.float),
-#     transforms.Normalize((0.5,), (0.5,))])
-
-#   transforms = []
-#     transforms.append(PILToTensor())
-#     transforms.append(ConvertImageDtype(torch.float))
-#     if train:
-#         transforms.append(RandomHorizontalFlip(0.5))
-#     return Compose(transforms)
-
 
 transform = transforms.Compose(
     [transforms.PILToTensor(),
      transforms.ConvertImageDtype(torch.float)
     ])
 
-
-
-
-# transform = transforms.Compose(
-#     [transforms.PILToTensor(),
-#      transforms.get_transform.PILToTensor
-#     transforms.Normalize((0.5,), (0.5,))])
-
 class FibreDataset(torch.utils.data.Dataset):
     def __init__(self, root, stage, transforms):
         self.root = root
         self.transforms = transforms
         self.stage = stage
-        # load all image files, sorting them to
-        # ensure that they are aligned
-        # self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
-        # self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
-        # self.imgs = list(sorted(os.listdir(os.path.join(root, 'Data', 'Prepared', 'Train', 'images'))))
-        # self.masks = list(sorted(os.listdir(os.path.join(root, 'Data', 'Prepared', 'Train', 'masks'))))        
         self.imgs = list(sorted(os.listdir(os.path.join(root, 'Prepared', self.stage, 'images'))))
         self.masks = list(sorted(os.listdir(os.path.join(root, 'Prepared', self.stage, 'masks'))))
 
     def __getitem__(self, idx):
         # load images and masks
+        
         img_path = os.path.join(self.root, 'Prepared', self.stage, 'images', self.imgs[idx])
         mask_path = os.path.join(self.root, 'Prepared', self.stage, 'masks', self.masks[idx])
+#        print(self.imgs[idx])
         img = Image.open(img_path).convert("RGB")
-        # note that we haven't converted the mask to RGB,
-        # because each color corresponds to a different instance
-        # with 0 being background
         mask = Image.open(mask_path)
-
-
-
-
-        
- 
-
-
-
         # convert the Image into a numpy array
         mask = np.array(mask)
         # Create an empty list to store the binary masks
         masks = []
-        
-        
-        # ##### DOS
-        # # Draw the contours on the image array
-        # cv2.drawContours(mask, contours, -1, (0, 255, 0), 2)
-
-        # # Convert the image array back to PIL Image
-        # image_with_contours = Image.fromarray(mask)
-
-
         
         # Get the contours of the image - to find the grid polygons
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
@@ -139,6 +89,12 @@ class FibreDataset(torch.utils.data.Dataset):
 
         # Iterate over each contour and update the masks array
         for i, contour in enumerate(contours):
+                # Check if the contour area is greater than 0
+            if cv2.contourArea(contour) == 0:
+                contour[-1][-1] += 1   # A straight line is being picked up as having no area 
+                                       # so this is a crude way of ensuring there are no 
+                                       # zero value areas.
+               
             # Create a new binary mask for the current contour
             contour_mask = np.zeros_like(mask, dtype=np.uint8)
             # Draw the contour on the binary mask
@@ -146,48 +102,12 @@ class FibreDataset(torch.utils.data.Dataset):
             # Update the masks array with the contour mask
             masks[i] = contour_mask
 
-         #   image_with_contours = Image.fromarray(contour_mask)
-            
-
-
-            # Draw the contours on the image array
-        #cv2.drawContours(mask, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
-
-        # Convert the image array back to PIL Image
-
-        
-        
-        
-        
-        # # Iterate over the contours and draw bounding boxes
-        # for contour in contours:
-        #     x, y, w, h = cv2.boundingRect(contour)
-        #     cv2.rectangle(mask, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        # Convert the image array back to PIL Image
-         #   image_with_boxes = Image.fromarray(mask)
-
-    #     # Iterate over each contour and plot it
-    #     for contour in contours:
-    #         # Create a new binary mask for the current contour
-    #         contour_mask = np.zeros_like(mask, dtype=np.uint8)     
-    #            # Draw the contour on the binary mask
-    #         cv2.drawContours(contour_mask, [contour], 0, (255), thickness=cv2.FILLED)
-    
-    # # Append the contour mask to the masks list
-    #         masks.append(contour_mask)       
-    
-        #num_objs = len(contours)
-        #boxes = torch.zeros([num_objs,4], dtype=torch.float32)
         boxes = [] 
         for i in range(num_objs):
             x,y,w,h = cv2.boundingRect(contours[i])
-           # boxes[i] = torch.tensor([x, y, x+w, y+h])
-            #boxes[i] = [x, y, x+w, y+h]
             boxes.append([x, y, x+w, y+h])
             
         # convert everything into a torch.Tensor
-       # boxes = torch.as_tensor(boxes, dtype=torch.float32)
         # there is only one class
         
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
@@ -199,31 +119,14 @@ class FibreDataset(torch.utils.data.Dataset):
         target["boxes"] = boxes
         target["labels"] = labels
         target["masks"] = masks
-       # target["image_id"] = image_id
-       # target["area"] = area
-        #target["iscrowd"] = iscrowd
 
-        
-        #sample = {'Image': img , 'target': target}
         if self.transforms is not None:
-        #     #sample = self.transforms(sample)
             img = self.transforms(img)
 
-        #data = [(img, target)]  # Create a list with a single tuple
-        
-        # Use the default_collate function to collate the data
-        #batch = default_collate(data)
-      #  batch = default_collate(resized_IMG, target)
-
-            
-        #return data
         return img, target
 
     def __len__(self):
         return len(self.imgs)    
-  
-
-
 
 def train_one_epoch(epoch_index, tb_writer):
     running_loss = 0.
@@ -238,28 +141,15 @@ def train_one_epoch(epoch_index, tb_writer):
         images1 = list(image.to(device) for image in inputs)
         targets1 = [{k: v.to(device) for k, v in t.items()} for t in labels]
 
- 
-        loss_dict = model(images1, targets1)
        # Zero your gradients for every batch!
-
+        optimizer.zero_grad()
+        loss_dict = model(images1, targets1)
 
         losses = sum(loss for loss in loss_dict.values())
         print(i,'loss:', losses.item())
-        optimizer.zero_grad()
+        
         losses.backward()
         optimizer.step()
-
-
-
-
-
-
-        # Compute the loss and its gradients
-        #loss = loss_fn(masks_pred, masks_target)
-        #loss.backward()
-
-        # Adjust learning weights
-        #optimizer.step()
 
         # Gather data and report
         running_loss += losses.item()
@@ -271,12 +161,6 @@ def train_one_epoch(epoch_index, tb_writer):
             running_loss = 0.
 
     return last_loss
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -292,16 +176,14 @@ if __name__ == "__main__":
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features,num_classes=2)  # replace the pre-trained head with a new one
     model.to(device)# move model to the right device
 
-    loss_fn = torch.nn.CrossEntropyLoss()
+    #loss_fn = torch.nn.CrossEntropyLoss()  # might be for multiclass classification
     current_directory = r'C:\Users\dezos\Documents\Fibres\FibreAnalysis\Data'
 
     # our dataset has two classes only - background and person
     num_classes = 2
     # use our dataset and defined transformations
-    # dataset = FibreDataset(current_directory, 'Train',  transform(train=True))
-    # dataset_test = FibreDataset(current_directory, 'Test', transform(train=False))
 
-    dataset = FibreDataset(current_directory, 'Train',  trainTransforms)     #get_transform(train=True)      transform
+    dataset = FibreDataset(current_directory, 'Train',  trainTransforms)    
     dataset_test = FibreDataset(current_directory, 'Test', valTransforms)
     
 
@@ -317,36 +199,7 @@ if __name__ == "__main__":
         dataset_test, batch_size=1, shuffle=False, num_workers=2,
         collate_fn=collate_fn)
 
-    #trainBatch = next(iter(data_loader))
-
-    #print(trainBatch)
-
-    # # define training and validation data loaders
-    # data_loader = torch.utils.data.DataLoader(
-    #     dataset, batch_size=2, shuffle=True, num_workers=2,
-    #     collate_fn=collate_fn)
-    #     #collate_fn=utils.collate_fn)
-
-    # data_loader_test = torch.utils.data.DataLoader(
-    #     dataset_test, batch_size=1, shuffle=False, num_workers=2,
-    #     collate_fn=collate_fn)
-    #     #collate_fn=utils.collate_fn)
-
-
-    # # Iterate over the data_loader
-    # for oneTry in data_loader:
-    #     print(oneTry)
-    #     # Perform operations on each batch of data
-    #     inputs, labels = batch_data
-    #     # ... your code here ...
-    #     # For example, you can pass the batch to your model for training or inference
-    #     print('One Loop')
-
-
     num_classes = 2
-
-
-
 
     optimizer = torch.optim.AdamW(params=model.parameters(), lr=1e-5)  # https://towardsdatascience.com/train-mask-rcnn-net-for-object-detection-in-60-lines-of-code-9b6bbff292c3
     # Optimizers specified in the torch.optim package 
@@ -359,7 +212,7 @@ if __name__ == "__main__":
     epoch_number = 0
 
     EPOCHS = 1  # Set number of epochs
-
+    loss_fn = torch.nn.BCEWithLogitsLoss
     best_vloss = 1_000_000.
 
     for epoch in range(EPOCHS):
@@ -382,19 +235,19 @@ if __name__ == "__main__":
                 images1 = list(image.to(device) for image in vinputs)
                 targets1 = [{k: v.to(device) for k, v in t.items()} for t in vlabels]            
                 voutputs = model(images1,targets1)
-                #vloss = loss_fn(voutputs, vlabels)
-                #running_vloss += vloss
+                vloss = loss_fn(voutputs, vlabels)
+                running_vloss += vloss
                 
                 
-                inputs, labels = vdata
-                images1 = list(image.to(device) for image in inputs)
-                targets1 = [{k: v.to(device) for k, v in t.items()} for t in labels]
-                voutputs = model(images1, )
+                # inputs, labels = vdata
+                # images1 = list(image.to(device) for image in inputs)
+                # targets1 = [{k: v.to(device) for k, v in t.items()} for t in labels]
+                # voutputs = model(images1, )
 
-        # Zero your gradients for every batch!
-        optimizer.zero_grad()
+        # # Zero your gradients for every batch!
+        # optimizer.zero_grad()
 
-        loss_dict = model(images1, targets1)
+        # loss_dict = model(images1, targets1)
                 
                 
 
